e9e5ebb543db17058aaf7fb3a3ccf9499c3fdc25
warmmark <zhongli.goldheart@gmail.com>
2024-05-31 15:25:17 +0300

    third task done

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function getDivisibleByThree(arrayOfNums) {
 console.log(getDivisibleByThree([1, 3, 5, 6, 7, 9, 11, 12]));
 
 function getEven(arrayOfNums) {
-    return arrayOfNums.filter((el) => el % 2 === 0);
+  return arrayOfNums.filter((el) => el % 2 === 0);
 }
 console.log(getEven([1, 3, 5, 6, 7, 9, 11, 12]));
+
+function firstLetterToUpperCase(arrayOfWords) {
+  return arrayOfWords.map((el) => `${el.slice(0, 1).toUpperCase()}${el.slice(1)}`).join(' ');
+}
+console.log(firstLetterToUpperCase(['В', 'лесу', 'родилась', 'ёлочка']));
