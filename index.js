32733e5a5981149981c3f70129df1d484ebb8ba5
warmmark <zhongli.goldheart@gmail.com>
2024-05-31 15:37:46 +0300

    fourth done

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function firstLetterToUpperCase(arrayOfWords) {
   return arrayOfWords.map((el) => `${el.slice(0, 1).toUpperCase()}${el.slice(1)}`).join(' ');
 }
 console.log(firstLetterToUpperCase(['В', 'лесу', 'родилась', 'ёлочка']));
+
+function changeFirstAndLastLetterOnEachOther(string) {
+  let result = string.split(' ')
+  .map((el) => el.length === 1 ? el : `${el[el.length - 1]}${el.slice(1, el.length - 1)}${el[0]}`)
+  .join(' ');
+  return result;
+}
+console.log(changeFirstAndLastLetterOnEachOther("В лесу родилась ёлочка"));
