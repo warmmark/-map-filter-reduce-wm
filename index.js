2a8bc1fe70a5cecc3300b56c2913363bd47d3308
warmmark <zhongli.goldheart@gmail.com>
2024-05-31 15:19:31 +0300

    second task done

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function isDivisibleByThree(arrayOfNums) {
+function getDivisibleByThree(arrayOfNums) {
   return arrayOfNums.filter((el) => el % 3 === 0);
 }
-console.log(isDivisibleByThree([1, 3, 5, 6, 7, 9, 11, 12]));
+console.log(getDivisibleByThree([1, 3, 5, 6, 7, 9, 11, 12]));
+
+function getEven(arrayOfNums) {
+    return arrayOfNums.filter((el) => el % 2 === 0);
+}
+console.log(getEven([1, 3, 5, 6, 7, 9, 11, 12]));
